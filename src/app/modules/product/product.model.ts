fad96ad9424003f6f6a57453a6c7d780801a8c9c
Abadat Hossain <abadat26@gmail.com>
2024-11-22 08:15:20 +0600

    successfully added timestamps for created times

--- src/app/modules/product/product.model.ts
+++ src/app/modules/product/product.model.ts
@@ -2,44 +2,49 @@
 import { Schema, model } from "mongoose";
 import { Product } from "./product.interface";
 
-const ProductSchema = new Schema<Product>({
-  name: {
-    type: String,
-    required: true,
+const ProductSchema = new Schema<Product>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    category: {
+      type: String,
+      required: true,
+      enum: [
+        "Writing",
+        "Office Supplies",
+        "Art Supplies",
+        "Educational",
+        "Technology",
+      ],
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    inStock: {
+      type: Boolean,
+      required: true,
+    },
   },
-  brand: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  category: {
-    type: String,
-    required: true,
-    enum: [
-      "Writing",
-      "Office Supplies",
-      "Art Supplies",
-      "Educational",
-      "Technology",
-    ],
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  quantity: {
-    type: Number,
-    required: true,
-    min: 0,
-  },
-  inStock: {
-    type: Boolean,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export const ProductModel = model<Product>("Product", ProductSchema);
--- src/app/modules/product/product.service.ts
+++ src/app/modules/product/product.service.ts
@@ -2,7 +2,11 @@ import { Product } from "./product.interface";
 import { ProductModel } from "./product.model";
 
 const createProductIntoDB = async (product: Product) => {
-  const result = await ProductModel.create(product);
+  const result = await ProductModel.create({
+    ...product,
+    createAt: new Date().toLocaleString(),
+    updateAt: new Date().toLocaleString(),
+  });
   return result;
 };
 
